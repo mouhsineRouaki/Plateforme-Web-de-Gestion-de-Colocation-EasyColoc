00fa55e0344e6b541adf951f3437e020bf70ea9a
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 15:00:38 +0000

    Fix Eloquent relationships in Invitation model

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = [
+        'colocation_id',
+        'invited_email',
+        'token',
+        'status',
+        'expires_at',
+        'sent_by',
+        'accepted_by',
+    ];
+
+    public function colocation(): BelongsTo{
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function sender(): BelongsTo{
+        return $this->belongsTo(User::class, 'sent_by');
+    }
+
+    public function accepter(): BelongsTo{
+        return $this->belongsTo(User::class, 'accepted_by');
+    }
 }
