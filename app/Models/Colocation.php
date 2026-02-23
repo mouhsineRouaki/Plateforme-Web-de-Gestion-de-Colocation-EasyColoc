a69c41e20e6a2bb2e9b768fb422a7e29cbad2ea0
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 15:00:38 +0000

    Fix Eloquent relationships in Colocation model

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -3,8 +3,51 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 class Colocation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'cancelled_at',
+        'created_by',
+    ];
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function members(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'colocation_user')->withPivot(['role_in_colocation', 'joined_at', 'left_at'])->withTimestamps();
+    }
+
+    public function invitations(): HasMany
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function expenses(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
+    public function reputationEvents(): HasMany
+    {
+        return $this->hasMany(ReputationEvent::class);
+    }
+
 }
