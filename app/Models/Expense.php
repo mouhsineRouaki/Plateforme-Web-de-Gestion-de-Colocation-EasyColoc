b33176c5a96604f6c71a21a10963231f58d45763
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 15:00:38 +0000

    Fix Eloquent relationships in Expense model

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'colocation_id', 'category_id', 'payer_id',
+        'title', 'amount', 'spent_at', 'note',
+    ];
+
+    public function colocation(): BelongsTo{
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category(): BelongsTo{
+        return $this->belongsTo(Category::class);
+    }
+
+    public function payer(): BelongsTo{
+        return $this->belongsTo(User::class, 'payer_id');
+    }
 }
