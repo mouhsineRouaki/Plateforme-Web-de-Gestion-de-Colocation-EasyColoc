c248c26fe5f209be0fae428dbfa3607c23e88fab
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 15:00:38 +0000

    Fix Eloquent relationships in Payment model

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'colocation_id', 'from_user_id', 'to_user_id',
+        'amount', 'paid_at', 'created_by',
+    ];
+
+     public function colocation(): BelongsTo{
+        return $this->belongsTo(Colocation::class);
+    }
+
+      public function fromUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    public function toUser(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+
+    public function createdBy(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
 }
