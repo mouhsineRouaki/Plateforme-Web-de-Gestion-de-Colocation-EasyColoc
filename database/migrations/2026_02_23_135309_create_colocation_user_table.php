72788ba75fa466d27c1a32081e143c4c386315f3
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 14:16:43 +0000

    Modification de la migration de la table pivot colocation_user

--- database/migrations/2026_02_23_135309_create_colocation_user_table.php
+++ database/migrations/2026_02_23_135309_create_colocation_user_table.php
@@ -4,22 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('colocation_user', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('colocation_id')->constrained('colocations')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+
+            $table->string('role_in_colocation')->default('MEMBER'); 
+            $table->timestamp('joined_at')->useCurrent();
+            $table->timestamp('left_at')->nullable();
+
             $table->timestamps();
+
+            $table->primary(['colocation_id', 'user_id']);
+            $table->index(['user_id', 'left_at']);
+            $table->index(['colocation_id', 'left_at']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('colocation_user');
