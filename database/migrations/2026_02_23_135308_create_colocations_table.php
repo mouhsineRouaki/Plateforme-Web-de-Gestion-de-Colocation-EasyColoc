41c16e582d22b10954cd93a262cdc671460af71b
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 19:26:55 +0000

    Mise à jour de la migration de la table colocations

--- database/migrations/2026_02_23_135308_create_colocations_table.php
+++ database/migrations/2026_02_23_135308_create_colocations_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
             $table->string('name');
             $table->text('description')->nullable();
             $table->string('status')->default('ACTIVE');
+            $table->string('image')->default('https://fourez.notaires.fr/uploads/notaires/decryptage-des-regles-pour-la-colocation-c6326cd4d7c438418a2c809cd44e0f87.jpeg');
             $table->timestamp('cancelled_at')->nullable();
             $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
