e3980ef174053ae06e89f5545061cfd81bbe29c0
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 14:16:43 +0000

    Modification de la migration de la table invitations

--- database/migrations/2026_02_23_135309_create_invitations_table.php
+++ database/migrations/2026_02_23_135309_create_invitations_table.php
@@ -4,22 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('invitations', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('colocation_id')->constrained('colocations')->cascadeOnDelete();
+
+            $table->string('invited_email');
+            $table->string('token')->unique();
+            $table->string('status')->default('PENDING'); // PENDING | ACCEPTED | REFUSED | EXPIRED | CANCELLED
+            $table->timestamp('expires_at');
+
+            $table->foreignId('sent_by')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('accepted_by')->nullable()->constrained('users')->nullOnDelete();
+
             $table->timestamps();
+
+            $table->index(['colocation_id', 'status']);
+            $table->index(['invited_email']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('invitations');
