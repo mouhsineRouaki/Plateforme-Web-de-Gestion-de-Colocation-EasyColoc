2854b1feb5f9d103f5037270246d31bd925fae3f
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 15:00:38 +0000

    Fix Eloquent relationships in ReputationEvent model

--- app/Models/ReputationEvent.php
+++ app/Models/ReputationEvent.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReputationEvent extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'colocation_id',
+        'type',
+        'delta',
+        'meta',
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation(): BelongsTo
+    {
+        return $this->belongsTo(Colocation::class);
+    }
 }
