edd706de52356a09dd02cdb836db341116ac5b4f
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 14:29:02 +0000

    Modification de la migration : création de la table categories

--- database/migrations/2026_02_23_135310_create_categories_table.php
+++ database/migrations/2026_02_23_135310_create_categories_table.php
@@ -10,12 +10,9 @@ return new class extends Migration {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('colocation_id')->constrained('colocations')->cascadeOnDelete();
-
             $table->string('name');
             $table->string('color')->nullable();
-
             $table->timestamps();
-
             $table->unique(['colocation_id', 'name']);
         });
     }
