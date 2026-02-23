dc2581b0f8a8c4d858ef3626ffd6018973a1e917
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 15:00:38 +0000

    Fix Eloquent relationships in Category model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['colocation_id', 'name', 'color'];
+
+    public function colocation():BelongsTo{
+        return $this->belongsTo(Colocation::class);
+    }
+    public function expense():BelongsTo{
+        return $this->belongsTo(Expense::class);
+    }
 }
