7e5b8c29340048f90ad54b0e01f534c0313efd21
Mouhsine Rouaki <houtm27@gmail.com>
2026-02-23 14:16:43 +0000

    Modification de la migration de la table colocations

--- database/migrations/2026_02_23_135308_create_colocations_table.php
+++ database/migrations/2026_02_23_135308_create_colocations_table.php
@@ -4,22 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('colocations', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('status')->default('ACTIVE');
+            $table->timestamp('cancelled_at')->nullable();
+            $table->foreignId('created_by')->constrained('users')->cascadeOnDelete();
             $table->timestamps();
+            $table->index(['status']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('colocations');
